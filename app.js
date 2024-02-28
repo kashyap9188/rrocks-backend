9a419e121dbf4f98dc72d1fdc34f4cf36f27a470
Kashyap Rajodiya <85735044+kashyap9188@users.noreply.github.com>
2024-02-29 02:03:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 const errorMiddleware = require("./middleware/error");
+const cors = require('cors');
 
 //config
 dotenv.config({path:"config/config.env"});
 
 app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:3000/', 'https://rrocks.netlify.app', 'https://master--rrocks.netlify.app']
+  })); 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
